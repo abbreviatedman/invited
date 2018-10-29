498c933ba4d10c5f65709f54766113e9f88bf4f8
abbreviatedman <colin.jaffe@gmail.com>
2018-10-29 19:51:12 -0400

    attempts to change classes dynamically

--- index.js
+++ index.js
@@ -26,14 +26,43 @@ const handleLocationChange = (event) => {
     document.querySelector('.location-preview').innerText = event.target.value
 }
 
+const checkStyle = () => {
+    const classList = document.querySelector('wedding-preview').classList;
+    if (classList.containts('blood')) {
+        return 'blood'
+    }
+}
+
+const switchToStyleOne = () => {
+    const weddingPreview = document.querySelector('.wedding-preview')
+    const classList = weddingPreview.classList;
+    if (!classList.contains('blood')) {
+        if (classList.contains('white-wedding')) {
+            weddingPreview.classList.replace('white-wedding', 'blood')
+        }
+    }
+}
+
+const switchToStyleTwo = () => {
+    const currentStyle = checkStyle();
+}
+
+const switchToStyleThree = () => {
+    const currentStyle = checkStyle();
+}
+
 function init() {
     document.addEventListener('DOMContentLoaded', () => {
-        document.querySelector('.first-name-input').addEventListener('keyup', handleFirstNameChange)
+            document.querySelector('.first-name-input').addEventListener('keyup', handleFirstNameChange)
         document.querySelector('.second-name-input').addEventListener('keyup', handleSecondNameChange)
         document.querySelector('.first-surname-input').addEventListener('keyup', handleFirstSurnameChange)
         document.querySelector('.second-surname-input').addEventListener('keyup', handleSecondSurnameChange)
         document.querySelector('.date-input').addEventListener('keyup', handleDateChange)
         document.querySelector('.location-input').addEventListener('keyup', handleLocationChange)
+
+        document.querySelector('.style-one').addEventListener('click', switchToStyleOne)
+        document.querySelector('.style-two').addEventListener('click', switchToStyleTwo)
+        document.querySelector('.style-three').addEventListener('click', switchToStyleThree)
     })
 }
 
